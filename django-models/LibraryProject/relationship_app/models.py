ae493f8e03fa26607bcbcd162ecd43c2afb61563
Pascal Mirimo <pascalmirimo57@gmail.com>
2025-09-07 07:10:59 -0700

    Task 4: Added URL patterns for permission-protected book views

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -22,7 +22,6 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey('Author', on_delete=models.CASCADE)
-    library = models.ForeignKey('Library', on_delete=models.CASCADE)
 
     class Meta:
         permissions = [
@@ -36,9 +35,11 @@ class Book(models.Model):
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name='libraries')
 
     def __str__(self):
         return self.name
+    
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -66,3 +66,19 @@ def librarian_view(request):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import get_object_or_404
+
+@permission_required('relationship_app.can_add_book')
+def add_book(request):
+    return render(request, 'relationship_app/add_book.html')
+
+@permission_required('relationship_app.can_change_book')
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'relationship_app/edit_book.html', {'book': book})
+
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'relationship_app/delete_book.html', {'book': book})
