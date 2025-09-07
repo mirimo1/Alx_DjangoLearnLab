a66c9a78886e4473d2cafe26f31721648e91d989
Pascal Mirimo <pascalmirimo57@gmail.com>
2025-09-07 05:52:43 -0700

    Fix: ensured UserCreationForm import and view structure for Task 2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from .models import Book, Library
 from django.views.generic.detail import DetailView
 
