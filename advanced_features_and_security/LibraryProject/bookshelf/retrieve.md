2a8c3efa490a473c602e02b38b1ac81dcc645f6f
Pascal Mirimo <pascalmirimo57@gmail.com>
2025-09-18 06:22:16 -0700

    Task 0: Implemented custom user model with manager, admin integration, and AUTH_USER_MODEL configuration

---
+++ advanced_features_and_security/LibraryProject/bookshelf/retrieve.md
@@ -0,0 +1,4 @@
+# Retrieve Book
+>>> book = Book.objects.get(title="1984")
+>>> book.title, book.author, book.publication_year
+('1984', 'George Orwell', 1949)
---
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.conf import settings  
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    added_by = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    on_delete=models.CASCADE,
+    related_name='relationship_books',
+    null=True,
+    blank=True
+)
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name='libraries')
+
+    def __str__(self):
+        return self.name
+    
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+
+    def __str__(self):
+        return self.name
