1b5a8efd2582172df03e2db38fa8bd2c98946a15
Pascal Mirimo <pascalmirimo57@gmail.com>
2025-09-18 05:19:45 -0700

    Task 2: Finalized authentication templates and imports

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import user_passes_test
 from .models import Book 
 from .models import Library 
