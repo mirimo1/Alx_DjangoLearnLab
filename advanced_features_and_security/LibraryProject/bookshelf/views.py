ea0c1f748444bda91bc99c902103220b7384a604
Pascal Mirimo <pascalmirimo57@gmail.com>
2025-09-20 02:20:21 -0700

    Task 1: Added Book model with permissions, views with decorators, and README documentation

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+@permission_required('bookshelf.can_view', raise_exception=True)
+def view_books(request):
+    books = Book.objects.all()
+    return render(request, 'books/view_books.html', {'books': books})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    # logic to create a book
+    pass
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    # logic to edit a book
+    pass
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    # logic to delete a book
+    pass
