8a09e9b74c35642d430545acb2275a6b94f0de3a
Pascal Mirimo <pascalmirimo57@gmail.com>
2025-10-12 16:45:26 -0700

    Fix: corrected STATIC_URL and completed TEMPLATES block for static file compliance

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -135,3 +135,52 @@ DATABASES = {
 }
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/profile/'
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL = '/static/'
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'blog' / 'static',
+]
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/profile/'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
