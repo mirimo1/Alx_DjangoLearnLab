6a41d108d389760d83bbcb70d80ede3c9e63e8ee
Pascal Mirimo <pascalmirimo57@gmail.com>
2025-10-25 21:54:32 -0700

    Add likes and notifications functionality

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -14,3 +14,12 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
+    
